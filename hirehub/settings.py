44b58deb9d3d1f6f85bd07d59fdae3ce5b1bb87a
Himanshi Sharma <88777492+himanshisharmaa@users.noreply.github.com>
2025-06-11 17:04:35 +0530

    Update settings.py

--- hirehub/settings.py
+++ hirehub/settings.py
@@ -83,7 +83,8 @@ import dj_database_url
 DATABASES={
     'default':dj_database_url.config(
         default=config('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
